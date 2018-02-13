9392a629263a0de061780052d5535bc02fae0b3b
devbaggett <devabaggett@gmail.com>
2018-02-12 17:04:24 -0800

    succesfully debugged

--- server.py
+++ server.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, session, flash
+from flask import Flask, render_template, request, redirect, session, flash
 app = Flask(__name__)
 app.secret_key = "unicorns"
 
@@ -11,14 +11,15 @@ app.secret_key = "unicorns"
 @app.route('/') 
 def index(): 
     
-
+   
 
     if "info" not in session:
         session['info'] = ""
     else:
-        for x in session['info']:
-            if x == "pizza":
-                x = "pumkin pie"
+        for x in range(0, len(session['info'])):
+            print x
+            if session['info'][x] == "pizza":
+                session['info'][x] = "pumkin pie"
     ##########################
     #Important!
     #Fix the above code.....
@@ -28,13 +29,17 @@ def index():
     return render_template("index.html", info = session['info'])
 
 
-@app.route("/form")
+@app.route("/form", methods=['POST'])
 def form():
     
-    if len(request.form['FirstName']) < 1 or len(request.form['Last_Name']) < 1:
+    print "In the post route"
+
+    if len(request.form['FirstName']) < 1 or len(request.form['LastName']) < 1:
+        print "length requirement not met"
         flash("Please Complete Form")
     else:
-        session['info'] = [request.form["FirstName"], request.form['Last_Name'], request.form['FaveSnack']]
+        print "length just fine"
+        session['info'] = [request.form["FirstName"], request.form['LastName'], request.form['FaveSnack']]
 
 
     return redirect('/')
